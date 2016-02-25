7816f6f3145f44424ca02eebd02f0995025ef633
kagawagao <kingsongao1221@gmail.com>
2016-02-25 14:18:48 +0800

    fix bug

--- src/components/tableBody.jsx
+++ src/components/tableBody.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
 import Row from './tableRow'
-import { Icon } from 'utils/antd'
+import {Icon} from 'antd'
 export default class TBody extends React.Component {
   static propTypes = {
     dataSource: PropTypes.array.isRequired,
@@ -15,16 +15,16 @@ export default class TBody extends React.Component {
   render () {
     const { dataSource, locale = {} } = this.props
     return (
-        <div className="nd-body">
-          {dataSource.length ? (
-            <div>
-              {dataSource.map((item, dataIndex) => (
+      <div className="nd-body">
+        {dataSource.length ? (
+          <div>
+            {dataSource.map((item, dataIndex) => (
               <Row key={dataIndex} data={item} {...this.props}/>
-              ))}
-            </div>) : (
-            <div><Icon type="frown" /> {locale.no_data || 'No Data'}</div>)
-        }
-        </div>
+            ))}
+          </div>) : (
+          <div><Icon type="frown" /> {locale.no_data || 'No Data'}</div>)
+      }
+      </div>
       )
   }
 }
--- src/components/tableHead.jsx
+++ src/components/tableHead.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
 import Checkbox from './checkbox'
-import { Icon } from 'utils/antd'
+import {Icon} from 'antd'
 export default class THead extends React.Component {
   static propTypes = {
     columns: PropTypes.array.isRequired,
@@ -40,31 +40,27 @@ export default class THead extends React.Component {
   }
   render () {
     const { columns, checkable = 0, checkedSet, dataSource, locale = {action: 'Action'}, itemActions = [] } = this.props
-    // let backgroundColor = '#f3f3f3'
-    // if (itemHeader.length > 0) {
-    //   backgroundColor = '#fff'
-    // }
     return (
-        <div className="nd-tableHeader">
-          <Checkbox {...this.props} checkable={checkable} checked={checkedSet.size === dataSource.length && !!dataSource.length}/>
-          {columns.map((item, index) => {
-            const className = item.span ? `nd-label span-${item.span}` : 'nd-label'
-            return (
-                      <div key={index} className={className}>
-                        <span>{item.label}</span>
-                        {item.sortable ? (
-                          <span className="nd-sortable">
-                            <Icon title={locale.asc || '升序'} className={this.state.active === 'asc' && this.state.name === item.name ? 'nd-active' : false} onClick={this.handleSort.bind(this, 'asc', item.name)} type="caret-up" />
-                            <Icon title={locale.desc || '降序'} className={this.state.active === 'desc' && this.state.name === item.name ? 'nd-active' : false} onClick={this.handleSort.bind(this, 'desc', item.name)} type="caret-down" />
-                          </span>
-                        ) : false}
-                      </div>)
-          })}
-            {itemActions.length ? (
-              <div className="nd-label">
+      <div className="nd-tableHeader">
+        <Checkbox {...this.props} checkable={checkable} checked={checkedSet.size === dataSource.length && !!dataSource.length}/>
+        {columns.map((item, index) => {
+          const className = item.span ? `nd-label span-${item.span}` : 'nd-label'
+          return (
+            <div key={index} className={className}>
+              <span>{item.label}</span>
+              {item.sortable ? (
+                <span className="nd-sortable">
+                  <Icon title={locale.asc || '升序'} className={this.state.active === 'asc' && this.state.name === item.name ? 'nd-active' : false} onClick={this.handleSort.bind(this, 'asc', item.name)} type="caret-up" />
+                  <Icon title={locale.desc || '降序'} className={this.state.active === 'desc' && this.state.name === item.name ? 'nd-active' : false} onClick={this.handleSort.bind(this, 'desc', item.name)} type="caret-down" />
+                </span>
+              ) : false}
+            </div>)
+        })}
+          {itemActions.length ? (
+            <div className="nd-label">
               <span>{locale.action}</span>
             </div>) : false}
-        </div>
+      </div>
       )
   }
 }
